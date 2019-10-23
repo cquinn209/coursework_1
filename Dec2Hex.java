73c40aaff5b93005b2e6ec0081296821a01b9a27
Ubuntu <cquinn209@DevOpsCW.y2wovfamktdujlqekusd4qy2oa.bx.internal.cloudapp.net>
2019-10-23 18:15:12 +0000

    BAD CODE

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -5,9 +5,9 @@ class Dec2Hex
 
      static int Arg1;
 
-    public static void main(String args[])
+    public static  ( args[])
     {
-        if (args.length > 0) 
+         (args.length > 0) 
         {
             try 
             {
