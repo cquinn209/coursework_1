4a6a722911788b1dee35caae50fe130ce14f9cd1
Ubuntu <cquinn209@DevOpsCW.y2wovfamktdujlqekusd4qy2oa.bx.internal.cloudapp.net>
2019-10-26 15:19:26 +0000

    working

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -25,15 +25,15 @@ class Dec2Hex
 	num = Arg1;
 	String hexadecimal=""; 
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
+        
         while(num != 0)
         {
 	    rem=num%16;
 	    hexadecimal= ch[rem] + hexadecimal;
             num= num/16;
         }
-
+        
         System.out.println("Hexadecimal representation is : " + hexadecimal);
- System.out.println("Hexadecimal representation is : testo lmao");
+
     }
 }     
