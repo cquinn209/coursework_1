ceb15ccf9de81be2dbfcf253670a58fa45d42657
Ubuntu <cquinn209@DevOpsCW.y2wovfamktdujlqekusd4qy2oa.bx.internal.cloudapp.net>
2019-10-28 19:45:12 +0000

    purposely wrong

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -3,14 +3,9 @@ import java.util.Scanner;
 class Dec2Hex
 {
 
-    public static int Arg1;
+    public stic int Arg1;
 
-    public static void main(String args[])
-    {
-        if (args.length > 0) 
-        {
-            try 
-            {
+    public sta
                 Arg1 = Integer.parseInt(args[0]);
             } 
             catch (NumberFormatException e) 
@@ -21,10 +16,10 @@ class Dec2Hex
         }
 
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-         int rem, num;
-	num = Arg1;
-	String hexadecimal=""; 
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+         in rem, num;
+	n = Arg1;
+	tring hexadecimal=""; 
+        stem.out.println("Converting the Decimal Value " + num + " to Hex...");
         
         while(num != 0)
         {
