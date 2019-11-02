45ce768c341b0810313e8fa787e017e6b9951e76
Ubuntu <cquinn209@DevOpsCW.y2wovfamktdujlqekusd4qy2oa.bx.internal.cloudapp.net>
2019-11-02 15:43:56 +0000

    code change

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -5,21 +5,45 @@ class Dec2Hex
 
     public static int Arg1;
 
+   
+    
     public static void main(String args[])
     {
+       
+      
+        
+  
+
+       
         if (args.length > 0) 
         {
             try 
             {
                 Arg1 = Integer.parseInt(args[0]);
-            } 
+   
+            }
+           
+          
             catch (NumberFormatException e) 
             {
-                System.err.println("Argument" + args[0] + " must be an integer.");
+                System.err.println("Argument " + args[0] + " must be an integer.");
                 System.exit(1);
             }
+      
+             
+           
+        
         }
 
+          if (Arg1 == 0) 
+                {
+                 System.err.println("Please enter a valid integer");
+                 System.exit(0);    
+                      }  
+                  
+        
+        
+        
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
          int rem, num;
 	num = Arg1;
